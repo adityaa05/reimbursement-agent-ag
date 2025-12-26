c1efe1bead3d6601fc94ef576a2ae93f3ab5d82a
adityaa05 <patiladityaa09@gmail.com>
2025-12-27 01:57:36 +0530

    Added Confluence configuration settings

--- config.py
+++ config.py
@@ -10,6 +10,15 @@ ODOO_DB = os.getenv("ODOO_DB")
 ODOO_USERNAME = os.getenv("ODOO_USERNAME")
 ODOO_PASSWORD = os.getenv("ODOO_PASSWORD")
 
+# Confluence Configuration
+CONFLUENCE_URL = os.getenv("CONFLUENCE_URL")
+CONFLUENCE_USERNAME = os.getenv("CONFLUENCE_USERNAME")
+CONFLUENCE_API_TOKEN = os.getenv("CONFLUENCE_API_TOKEN")
+CONFLUENCE_SPACE_KEY = os.getenv("CONFLUENCE_SPACE_KEY")
+CONFLUENCE_POLICY_PAGE_TITLE = os.getenv(
+    "CONFLUENCE_POLICY_PAGE_TITLE", "Travel & Expense Policy"
+)
+
 # AWS Textract Client
 textract_client = boto3.client(
     "textract",
